f399bcb81394a70a1e6014c4d2aab4743451c6e8
V S Sreejeet <vsreejeet@gmail.com>
2020-06-07 03:01:13 +0530

    Completed stage 2

--- main.go
+++ main.go
@@ -1,24 +1,85 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	handler := http.HandlerFunc(Echo)
+
 	const serveURL string = "localhost:8000"
 
-	log.Printf("Server started. Listening at %s\n", serveURL)
-	defer log.Print("Server stopped.")
+	// Basic logging
+	log.Printf("Starting service")
+	defer log.Print("Ending service")
 
-	if err := http.ListenAndServe(serveURL, handler); err != nil {
-		log.Fatalf("ERROR: %s\n", err)
+	// Create api as a http.Server
+	api := http.Server{
+		Addr:         serveURL,
+		Handler:      http.HandlerFunc(Echo),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	// A channel to listen for errors from the server.
+	// A buffer is used so that the goroutine can safely exit
+	// if we fail to collect the error.
+	serverErrors := make(chan error, 1)
+
+	// Here we start the server for the (micro)service
+	go func() {
+		log.Printf("Server started at %s\n", serveURL)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	// Another channel to recieve OS signals like SIGINT or SIGTERM.
+	// The signal package requires this channel to be buffered.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	// Using the switch case construct, or in case of Go,
+	// the select construct to block main func till shutdown
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Error listening: %s\n", err)
+	case <-shutdown:
+		log.Printf("Shutting down service")
+
+		// Deadline for finishing any outstanding requests
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		// Asking listener to shut down
+		err := api.Shutdown(ctx)
+		if err != nil {
+			log.Printf("Could not gracefully shut down server in %v : %v", timeout, err)
+			err = api.Close()
+		}
+		if err != nil {
+			log.Fatalf("Could not stop server gracefully : %v", err)
+		}
+
 	}
 }
 
 // Echo is a basic http handler
 func Echo(w http.ResponseWriter, r *http.Request) {
+
+	// Printing reandom numbers at begining and emd of each request
+	n := rand.Intn(10000)
+	log.Println("Start", n)
+	defer log.Println("End", n)
+
+	// Simulate long running request
+	time.Sleep(3 * time.Second)
+
 	fmt.Fprintf(w, "Performed %s %s\n", r.Method, r.URL.Path)
 }
