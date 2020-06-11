c6fddc3422de65044661e16f167c89339cbd2910
V S Sreejeet <vsreejeet@gmail.com>
2020-06-11 22:44:57 +0530

    Moved schema to internal/schema. Introduced internal/product package

---
+++ internal/product/models.go
@@ -0,0 +1,12 @@
+package products
+
+import "time"
+
+type Product struct {
+	ID          string    `db:"product_id" json:"id"`
+	Name        string    `db:"name" json:"name"`
+	Cost        int       `db:"cost" json:"cost"`
+	Quantity    int       `db:"quantity" json:"quantity"`
+	DateCreated time.Time `db:"date_created" json:"date_created"`
+	DateUpdated time.Time `db:"date_updated" json:"date_updated"`
+}
