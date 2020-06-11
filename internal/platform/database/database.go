4f200fb95baf4e795b6db555420f5759c60474a8
V S Sreejeet <vsreejeet@gmail.com>
2020-06-11 23:12:32 +0530

    created /internal/platform/database pkg

---
+++ internal/platform/database/database.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"net/url"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pkg" // The drier being used
+)
+
+// Open workds as an abstraction to open a database conn
+func Open() (*sqlx.DB, error) {
+	query := make(url.Values)
+	query.Set("sslmode", "disable")
+	query.Set("timezone", "utc")
+
+	url := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword("postgres", "postgres"),
+		Host:     "localhost",
+		Path:     "postgres",
+		RawQuery: q.Encode(),
+	}
+}
