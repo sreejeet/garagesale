1223d0dcb6a1551bd16feb1b8cb1bcecbdbc5513
V S Sreejeet <vsreejeet@gmail.com>
2020-06-13 22:53:09 +0530

    mod sales-admin/main.go to support configuration

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/sreejeet/garagesale/internal/platform/conf"
 	"github.com/sreejeet/garagesale/internal/platform/database"
 	"github.com/sreejeet/garagesale/internal/schema"
 )
 
 func main() {
-	flag.Parse()
+
+	var cfg struct {
+		DB struct {
+			User       string `conf:"default:postgres"`
+			Password   string `conf:"default:postgres,noprint"`
+			Host       string `conf:"default:localhost"`
+			Name       string `conf:"default:postgres"`
+			DisableTLS bool   `conf:"default:false"`
+		}
+		Args conf.Args
+	}
+
+	if err := conf.Parse(os.Args[1:], "SALES", &cfg); err != nil {
+		if err == conf.ErrHelpWanted {
+			usage, err := conf.Usage("SALES", &cfg)
+			if err != nil {
+				log.Fatalf("Error generating usage for database: %s\n", err)
+			}
+			fmt.Println(usage)
+			return
+		}
+		log.Fatalf("Error parsing database configuration: %s\n", err)
+	}
 
 	// Open database connection for performing actions
-	db, err := database.Open()
+	db, err := database.Open(database.Config{
+		User:       cfg.DB.User,
+		Password:   cfg.DB.Password,
+		Host:       cfg.DB.Host,
+		Name:       cfg.DB.Name,
+		DisableTLS: cfg.DB.DisableTLS,
+	})
 	if err != nil {
 		log.Fatalf("error connecting to database: %s\n", err)
 	}
 	defer db.Close()
 
-	switch flag.Arg(0) {
+	switch cfg.Args.Num(0) {
 
 	case "migrate":
 		// Migrate database schema
