7e0351422141108859e012dcc338106fd240ddf9
V S Sreejeet <vsreejeet@gmail.com>
2020-06-15 23:03:45 +0530

    using Log pointer instead of pg level variable

--- cmd/sales-api/internal/handlers/product.go
+++ cmd/sales-api/internal/handlers/product.go
@@ -12,7 +12,8 @@ import (
 // Products define all handlers for products. It also holds
 // the application state needed by the handler methods.
 type Products struct {
-	DB *sqlx.DB
+	DB  *sqlx.DB
+	Log *log.Logger
 }
 
 // List is an http handler for returning
@@ -20,7 +21,7 @@ type Products struct {
 func (p *Products) List(w http.ResponseWriter, r *http.Request) {
 	list, err := product.List(p.DB)
 	if err != nil {
-		log.Printf("error listing products: %s\n", err)
+		p.Log.Printf("error listing products: %s\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -28,14 +29,14 @@ func (p *Products) List(w http.ResponseWriter, r *http.Request) {
 	// Marshalling (converting) product slice to json string
 	data, err := json.Marshal(list)
 	if err != nil {
-		log.Print("Error parsing json:", err)
+		p.Log.Print("Error parsing json:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	if _, err := w.Write(data); err != nil {
-		log.Print("Error writing json:", err)
+		p.Log.Print("Error writing json:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
