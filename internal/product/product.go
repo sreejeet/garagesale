d676b8203b42b18ccfa33cf40b789ec82d02300f
V S Sreejeet <vsreejeet@gmail.com>
2020-06-16 22:25:43 +0530

    added func to get single product

--- internal/product/product.go
+++ internal/product/product.go
@@ -14,3 +14,14 @@ func List(db *sqlx.DB) ([]Product, error) {
 	}
 	return products, nil
 }
+
+// Retrieve is used to get a single product based on its ID from the URL parameter.
+func Retrieve(db *sqlx.DB) (*Product, error) {
+	var p Product
+
+	const query = `SELECT * FROM products WHERE product_id = $1`
+	if err := db.Get(&p, query); err != nil {
+		return nil, errors.Wrap(err, "selecting one product")
+	}
+	return &p, nil
+}
