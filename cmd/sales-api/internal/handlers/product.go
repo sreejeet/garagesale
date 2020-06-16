68c44e86df81e66d5a1f9ac1258d6d560fc55742
V S Sreejeet <vsreejeet@gmail.com>
2020-06-16 23:00:37 +0530

    added support for getting single product

--- cmd/sales-api/internal/handlers/product.go
+++ cmd/sales-api/internal/handlers/product.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/go-chi/chi"
 	"github.com/jmoiron/sqlx"
 	"github.com/sreejeet/garagesale/internal/product"
 )
@@ -12,16 +13,16 @@ import (
 // Products define all handlers for products. It also holds
 // the application state needed by the handler methods.
 type Products struct {
-	DB  *sqlx.DB
-	Log *log.Logger
+	db  *sqlx.DB
+	log *log.Logger
 }
 
 // List is an http handler for returning
 // a json list of products.
 func (p *Products) List(w http.ResponseWriter, r *http.Request) {
-	list, err := product.List(p.DB)
+	list, err := product.List(p.db)
 	if err != nil {
-		p.Log.Printf("error listing products: %s\n", err)
+		p.log.Printf("error listing products: %s\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -29,14 +30,40 @@ func (p *Products) List(w http.ResponseWriter, r *http.Request) {
 	// Marshalling (converting) product slice to json string
 	data, err := json.Marshal(list)
 	if err != nil {
-		p.Log.Print("Error parsing json:", err)
+		p.log.Print("Error parsing json:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	if _, err := w.Write(data); err != nil {
-		p.Log.Print("Error writing json:", err)
+		p.log.Print("Error writing json:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+// Retrieve is used to get a single product based on its ID from the URL parameter.
+func (p *Products) Retrieve(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+	prod, err := product.Retrieve(p.db, id)
+	if err != nil {
+		p.log.Printf("error finding product: %s\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Marshalling (converting) product slice to json string
+	data, err := json.Marshal(prod)
+	if err != nil {
+		p.log.Print("Error parsing json:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if _, err := w.Write(data); err != nil {
+		p.log.Print("Error writing json:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
