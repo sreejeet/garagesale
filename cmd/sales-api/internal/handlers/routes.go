918ffefc9406f8a710169c4e042946b6f740220e
V S Sreejeet <vsreejeet@gmail.com>
2020-06-17 23:40:01 +0530

    added route to create product

--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -20,6 +20,7 @@ func API(db *sqlx.DB, log *log.Logger) http.Handler {
 
 	app.Handle(http.MethodGet, "/v1/products", p.List)
 	app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
+	app.Handle(http.MethodPost, "/v1/products", p.Create)
 
 	return app
 }
