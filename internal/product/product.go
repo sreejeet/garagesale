3cb86297499c3828d8c166924bde19038c10ced7
V S Sreejeet <vsreejeet@gmail.com>
2020-06-17 23:25:49 +0530

    Added create func for products

--- internal/product/product.go
+++ internal/product/product.go
@@ -1,6 +1,9 @@
 package product
 
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 )
@@ -23,5 +26,32 @@ func Retrieve(db *sqlx.DB, id string) (*Product, error) {
 	if err := db.Get(&prod, query, id); err != nil {
 		return nil, errors.Wrap(err, "selecting one product")
 	}
+	return &prod, nil
+}
+
+// Create creates a new product int the database and return the created product.
+func Create(db *sqlx.DB, newProd NewProduct, now time.Time) (*Product, error) {
+	prod := Product{
+		ID:          uuid.New().String(),
+		Name:        newProd.Name,
+		Cost:        newProd.Cost,
+		Quantity:    newProd.Quantity,
+		DateCreated: now.UTC(),
+		DateUpdated: now.UTC(),
+	}
+
+	const query = `INSERT INTO products
+		(product_id, name, cost, quantity, date_created, date_updated)
+		VALUES ($1, $2, $3, $4, $5, $6)`
+
+	_, err := db.Exec(query,
+		prod.ID, prod.Name,
+		prod.Cost, prod.Quantity,
+		prod.DateCreated, prod.DateUpdated)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "Creating new product")
+	}
+
 	return &prod, nil
 }
