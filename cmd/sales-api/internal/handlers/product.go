9faecd4cb891438611513be78bdd0d3678ccd19f
V S Sreejeet <vsreejeet@gmail.com>
2020-06-17 23:38:27 +0530

    created handler for creating product

--- cmd/sales-api/internal/handlers/product.go
+++ cmd/sales-api/internal/handlers/product.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/jmoiron/sqlx"
@@ -57,6 +58,41 @@ func (p *Products) Retrieve(w http.ResponseWriter, r *http.Request) {
 	data, err := json.Marshal(prod)
 	if err != nil {
 		p.log.Println("Error parsing json:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if _, err := w.Write(data); err != nil {
+		p.log.Println("Error writing json:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+// Create is used to create a new product from the body of a request.
+// The created product is sent back to the client
+// in conformance to the RESTful architecture.
+func (p *Products) Create(w http.ResponseWriter, r *http.Request) {
+
+	var newProd product.NewProduct
+
+	// Decoding response body to NewProduct struct
+	if err := json.NewDecoder(r.Body).Decode(&newProd); err != nil {
+		p.log.Println("Error decoding response:", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	prod, err := product.Create(p.db, newProd, time.Now())
+	if err != nil {
+		p.log.Println("Error creating product:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(prod)
+	if err != nil {
+		p.log.Println("Error marshalling result:", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
