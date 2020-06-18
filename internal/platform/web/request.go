2484f550a31616e9ce163ffa2e271bef60ce0946
V S Sreejeet <vsreejeet@gmail.com>
2020-06-18 23:12:32 +0530

    created request json decoder

---
+++ internal/platform/web/request.go
@@ -0,0 +1,14 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Decode function decodes a json response into the provided value.
+func Decode(r *http.Request, val interface{}) error {
+	if err := json.NewDecoder(r.Body).Decode(val); err != nil {
+		return err
+	}
+	return nil
+}
