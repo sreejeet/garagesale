3219ee5cbce2eafc066628ea576788cef13072a3
V S Sreejeet <vsreejeet@gmail.com>
2020-06-26 23:12:35 +0530

    product methods now use context

--- internal/product/product.go
+++ internal/product/product.go
@@ -1,6 +1,7 @@
 package product
 
 import (
+	"context"
 	"database/sql"
 	"time"
 
@@ -12,18 +13,18 @@ import (
 // Custom errors for expected failing conditions
 var (
 	// Invalid UUID
-	ErrInvalidID = errors.New("Invalid ID!")
+	ErrInvalidID = errors.New("invalid ID!")
 	// Unable to find product based on UUID
-	ErrNotFound = errors.New("Product not found!")
+	ErrNotFound = errors.New("product not found!")
 )
 
 // List retrieves all products from the database
-func List(db *sqlx.DB) ([]Product, error) {
+func List(ctx context.Context, db *sqlx.DB) ([]Product, error) {
 
 	products := []Product{}
 	const query = `SELECT * FROM products`
 
-	if err := db.Select(&products, query); err != nil {
+	if err := db.SelectContext(ctx, &products, query); err != nil {
 		return nil, errors.Wrap(err, "selecting products")
 	}
 
@@ -31,7 +32,7 @@ func List(db *sqlx.DB) ([]Product, error) {
 }
 
 // Retrieve is used to get a single product based on its ID from the URL parameter.
-func Retrieve(db *sqlx.DB, id string) (*Product, error) {
+func Retrieve(ctx context.Context, db *sqlx.DB, id string) (*Product, error) {
 
 	// Check for invalid UUID
 	if _, err := uuid.Parse(id); err != nil {
@@ -41,7 +42,7 @@ func Retrieve(db *sqlx.DB, id string) (*Product, error) {
 	var prod Product
 
 	const query = `SELECT * FROM products WHERE product_id = $1`
-	if err := db.Get(&prod, query, id); err != nil {
+	if err := db.GetContext(ctx, &prod, query, id); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, ErrNotFound
 		}
@@ -52,7 +53,7 @@ func Retrieve(db *sqlx.DB, id string) (*Product, error) {
 }
 
 // Create creates a new product int the database and return the created product.
-func Create(db *sqlx.DB, newProd NewProduct, now time.Time) (*Product, error) {
+func Create(ctx context.Context, db *sqlx.DB, newProd NewProduct, now time.Time) (*Product, error) {
 
 	prod := Product{
 		ID:          uuid.New().String(),
@@ -67,7 +68,7 @@ func Create(db *sqlx.DB, newProd NewProduct, now time.Time) (*Product, error) {
 		(product_id, name, cost, quantity, date_created, date_updated)
 		VALUES ($1, $2, $3, $4, $5, $6)`
 
-	_, err := db.Exec(query,
+	_, err := db.ExecContext(ctx, query,
 		prod.ID, prod.Name,
 		prod.Cost, prod.Quantity,
 		prod.DateCreated, prod.DateUpdated)
