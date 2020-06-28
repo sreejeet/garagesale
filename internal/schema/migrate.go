ba5f9caf6f4b68e28a7019a71b894cb9af9d31a5
V S Sreejeet <vsreejeet@gmail.com>
2020-06-28 22:25:33 +0530

    added sales table migrations

--- internal/schema/migrate.go
+++ internal/schema/migrate.go
@@ -14,15 +14,26 @@ var migrations = []darwin.Migration{
 		Version:     1,
 		Description: "Add products",
 		Script: `CREATE TABLE products (
-			product_id UUID,
-			name TEXT,
-			cost INT,
-			quantity INT,
-			date_created TIMESTAMP,
-			date_updated TIMESTAMP,
-
-			PRIMARY KEY (product_id)
+					product_id UUID,
+					name TEXT,
+					cost INT,
+					quantity INT,
+					date_created TIMESTAMP,
+					date_updated TIMESTAMP,
+					PRIMARY KEY (product_id)
 		);`,
+	}, {
+		Version:     2,
+		Description: "Add sales",
+		Script: `CREATE TABLE sales (
+					sale_id UUID,
+					product_id UUID,
+					quantity INT,
+					paid INT,
+					date_created TIMESTAMP,
+					PRIMARY KEY (sale_id),
+					FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+				);`,
 	},
 }
 
