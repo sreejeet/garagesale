1028eb08898a4c6bdbe8cb93e0435af86128d70e
V S Sreejeet <vsreejeet@gmail.com>
2020-07-02 23:29:58 +0530

    added route for updating products

--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -22,6 +22,7 @@ func API(db *sqlx.DB, log *log.Logger) http.Handler {
 	app.Handle(http.MethodGet, "/v1/products", p.List)
 	app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
 	app.Handle(http.MethodPost, "/v1/products", p.Create)
+	app.Handle(http.MethodPut, "/v1/products/{id}", p.Update)
 
 	// Sale specific routes
 	app.Handle(http.MethodPost, "/v1/products/{id}/sales", p.AddSale)
