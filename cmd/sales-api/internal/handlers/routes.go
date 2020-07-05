de117e4b377ae80dd3a90e302e9b2ac3f906a42e
V S Sreejeet <vsreejeet@gmail.com>
2020-07-05 22:05:38 +0530

    added route for health check

--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -13,21 +13,30 @@ func API(db *sqlx.DB, log *log.Logger) http.Handler {
 
 	app := web.NewApp(log)
 
-	p := Products{
-		db:  db,
-		log: log,
+	{
+		c := Check{db: db}
+
+		// Health check route
+		app.Handle(http.MethodGet, "/v1/health", c.Health)
 	}
 
-	// Product specific routes
-	app.Handle(http.MethodGet, "/v1/products", p.List)
-	app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
-	app.Handle(http.MethodPost, "/v1/products", p.Create)
-	app.Handle(http.MethodPut, "/v1/products/{id}", p.Update)
-	app.Handle(http.MethodDelete, "/v1/products/{id}", p.Delete)
+	{
+		p := Products{
+			db:  db,
+			log: log,
+		}
 
-	// Sale specific routes
-	app.Handle(http.MethodPost, "/v1/products/{id}/sales", p.AddSale)
-	app.Handle(http.MethodGet, "/v1/products/{id}/sales", p.ListSales)
+		// Product specific routes
+		app.Handle(http.MethodGet, "/v1/products", p.List)
+		app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
+		app.Handle(http.MethodPost, "/v1/products", p.Create)
+		app.Handle(http.MethodPut, "/v1/products/{id}", p.Update)
+		app.Handle(http.MethodDelete, "/v1/products/{id}", p.Delete)
+
+		// Sale specific routes
+		app.Handle(http.MethodPost, "/v1/products/{id}/sales", p.AddSale)
+		app.Handle(http.MethodGet, "/v1/products/{id}/sales", p.ListSales)
+	}
 
 	return app
 }
