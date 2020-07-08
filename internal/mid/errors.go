1173c549d600990c32a5f353fe9f4235e355b30b
V S Sreejeet <vsreejeet@gmail.com>
2020-07-08 10:02:06 +0530

    added middleware error handling

---
+++ internal/mid/errors.go
@@ -0,0 +1,41 @@
+package mid
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/sreejeet/garagesale/internal/platform/web"
+)
+
+// Errors handles errors from the middleware chains and
+// respond to normal application errors in a uniform manner.
+// Any unexpected errors will be responded with a 5xx status code
+// and will be logged.
+func Errors(log *log.Logger) web.Middleware {
+
+	// This is the actual middleware function to be executed.
+	f := func(before web.Handler) web.Handler {
+
+		h := func(w http.ResponseWriter, r *http.Request) error {
+
+			// Run the handler chain and catch any propagated error.
+			if err := before(w, r); err != nil {
+
+				// Log the error.
+				log.Printf("ERROR : %v", err)
+
+				// Respond to the error.
+				if err := web.RespondError(w, err); err != nil {
+					return err
+				}
+			}
+
+			// Return nil to indicate the error has been handled.
+			return nil
+		}
+
+		return h
+	}
+
+	return f
+}
