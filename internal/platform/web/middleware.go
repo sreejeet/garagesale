a78f66d5ddb33c68d6d5aba9a4aefce8919b38a4
V S Sreejeet <vsreejeet@gmail.com>
2020-07-08 08:46:59 +0530

    added middleware package

---
+++ internal/platform/web/middleware.go
@@ -0,0 +1,24 @@
+package web
+
+// Middleware type runs some code before or after a Handler and calls
+// the next Handler in the middleware list. This moves generic or boilerplate
+// code away from the top level handlers.
+type Middleware func(Handler) Handler
+
+// wrapMiddleware creates a new handler by wrapping middleware around a final
+// handler. The middlewares' Handlers will be executed by requests in the order
+// they are provided.
+func wrapMiddleware(mw []Middleware, handler Handler) Handler {
+
+	// Loop backwards through the middleware invoking each one. Replace the
+	// handler with the new wrapped handler. Looping backwards ensures that the
+	// first middleware of the slice is the first to be executed by requests.
+	for i := len(mw) - 1; i >= 0; i-- {
+		h := mw[i]
+		if h != nil {
+			handler = h(handler)
+		}
+	}
+
+	return handler
+}
