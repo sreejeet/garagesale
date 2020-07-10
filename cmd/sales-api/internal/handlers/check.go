2ed15fd764153eff577b887e30041ecf918da7b2
V S Sreejeet <vsreejeet@gmail.com>
2020-07-10 21:10:38 +0530

    context part 2
    modified func signatures for check handler

--- cmd/sales-api/internal/handlers/check.go
+++ cmd/sales-api/internal/handlers/check.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/jmoiron/sqlx"
@@ -15,22 +16,22 @@ type Check struct {
 
 // Health provides a high level overview of the service status
 // and lets the client know if and why it is [not]capable of taking requests
-func (c *Check) Health(w http.ResponseWriter, r *http.Request) error {
+func (c *Check) Health(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
 	var health struct {
 		DBStatus string `json:"db_status"`
 	}
 
 	// Check if the database is ready.
-	if err := database.StatusCheck(r.Context(), c.db); err != nil {
+	if err := database.StatusCheck(ctx, c.db); err != nil {
 
 		// If the database is not ready we will respond to the client with a 500 status.
 		// Returning the error as it is will result in an unhandled exception,
 		// send a json web response insetead.
 		health.DBStatus = "The database is not accpeting requests at the moment."
-		return web.Respond(r.Context(), w, health, http.StatusInternalServerError)
+		return web.Respond(ctx, w, health, http.StatusInternalServerError)
 	}
 
 	health.DBStatus = "ok"
-	return web.Respond(r.Context(), w, health, http.StatusOK)
+	return web.Respond(ctx, w, health, http.StatusOK)
 }
