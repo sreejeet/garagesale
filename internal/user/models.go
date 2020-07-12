b704e6c90a232fa880fca272bd6d1cbaa96ff692
V S Sreejeet <vsreejeet@gmail.com>
2020-07-12 22:56:47 +0530

    useradd
    created user models

---
+++ internal/user/models.go
@@ -0,0 +1,27 @@
+package user
+
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
+
+// User model for any internal user of the system.
+type User struct {
+	ID           string         `db:"user_id" json:"id"`
+	Name         string         `db:"name" json:"name"`
+	Email        string         `db:"email" json:"email"`
+	Roles        pq.StringArray `db:"roles" json:"roles"`
+	PasswordHash []byte         `db:"password_hash" json:"-"`
+	DateCreated  time.Time      `db:"date_created" json:"date_created"`
+	DateUpdated  time.Time      `db:"date_updated" json:"date_updated"`
+}
+
+// NewUser is the form sent by the client for creating new users
+type NewUser struct {
+	Name            string   `json:"name" validate:"required"`
+	Email           string   `json:"email" validate:"required"`
+	Roles           []string `json:"roles" validate:"required"`
+	Password        string   `json:"password" validate:"required"`
+	PasswordConfirm string   `json:"password_confirm" validate:"eqfield=Password"`
+}
