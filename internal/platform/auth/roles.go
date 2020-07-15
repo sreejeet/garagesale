85b10a7f4483d242200c40729e849450c0719a00
V S Sreejeet <vsreejeet@gmail.com>
2020-07-15 23:21:58 +0530

    JWT Auth
    added claim creation funcs

--- internal/platform/auth/roles.go
+++ internal/platform/auth/roles.go
@@ -1,7 +1,35 @@
 package auth
 
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
 // Here we define the roles a user can have.
 const (
 	RoleAdmin = "ADMIN"
 	RoleUser  = "USER"
 )
+
+// Claims is the payload of JWTs.
+type Claims struct {
+	Roles []string `json:"roles"`
+	jwt.StandardClaims
+}
+
+// NewClaims creates a new Claims object for the identified user. Additional fields can be
+// set after creating this Claim.
+func NewClaims(subject string, roles []string, now time.Time, expires time.Duration) Claims {
+
+	c := Claims{
+		Roles: roles,
+		StandardClaims: jwt.StandardClaims{
+			Subject:   subject,
+			IssuedAt:  now.Unix(),
+			ExpiresAt: now.Add(expires).Unix(),
+		},
+	}
+
+	return c
+}
