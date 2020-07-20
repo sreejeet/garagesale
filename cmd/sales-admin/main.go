471db7ff233225ad00dbd72f56b52b446a756787
V S Sreejeet <vsreejeet@gmail.com>
2020-07-20 19:00:23 +0530

    Login
    Create private key and save to file in path

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"log"
 	"os"
@@ -64,6 +68,8 @@ func run() error {
 		err = seed(dbConfig)
 	case "useradd":
 		err = useradd(dbConfig, cfg.Args.Num(1), cfg.Args.Num(2))
+	case "keygen":
+		err = keygen(cfg.Args.Num(1))
 	default:
 		err = errors.New("Must specify a command")
 	}
@@ -150,5 +156,39 @@ func useradd(cfg database.Config, email, password string) error {
 	}
 
 	fmt.Println("Administrator created with id:", u.ID)
+	return nil
+}
+
+// keygen creates an x509 private key for signing auth tokens.
+func keygen(path string) error {
+
+	if path == "" {
+		return errors.New("keygen missing argument for key path")
+	}
+
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return errors.Wrap(err, "generating keys")
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return errors.Wrap(err, "creating private key file")
+	}
+	defer file.Close()
+
+	block := pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	}
+
+	if err := pem.Encode(file, &block); err != nil {
+		return errors.Wrap(err, "encoding to private key file")
+	}
+
+	if err := file.Close(); err != nil {
+		return errors.Wrap(err, "closing private key file")
+	}
+
 	return nil
 }
