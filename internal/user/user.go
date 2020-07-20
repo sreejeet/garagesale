8b032224e36b380cf08c55043289f62007a5bff4
V S Sreejeet <vsreejeet@gmail.com>
2020-07-20 12:16:53 +0530

    Login
    Added authentication for users

--- internal/user/user.go
+++ internal/user/user.go
@@ -2,12 +2,20 @@ package user
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/bcrypt"
+
+	"github.com/sreejeet/garagesale/internal/platform/auth"
+)
+
+var (
+	// ErrAuthenticationFailure is used to indicate any kind of authentication error.
+	ErrAuthenticationFailure = errors.New("Authentication failed")
 )
 
 // Create is used to create a new user.
@@ -43,4 +51,35 @@ func Create(ctx context.Context, db *sqlx.DB, n NewUser, now time.Time) (*User, 
 	}
 
 	return &u, nil
+}
+
+// Authenticate searches the database for a user based on the email, verifies the password
+// and if this succeeds, a Claims object is returned. This Claims object is used to
+// create a token for future authenctications.
+func Authenticate(ctx context.Context, db *sqlx.DB, now time.Time, email, password string) (auth.Claims, error) {
+
+	// Query for email search
+	const q = `SELECT * FROM users WHERE email = $1`
+
+	var u User
+	if err := db.GetContext(ctx, &u, q, email); err != nil {
+
+		// Normally we would return ErrNotFound in this scenario but we do not want
+		// to leak to an unauthenticated user which emails are in the system.
+		if err == sql.ErrNoRows {
+			return auth.Claims{}, ErrAuthenticationFailure
+		}
+
+		return auth.Claims{}, errors.Wrap(err, "selecting single user")
+	}
+
+	// Compare the provided password with the saved hash. Use the bcrypt
+	// comparison function so it is cryptographically secure.
+	if err := bcrypt.CompareHashAndPassword(u.PasswordHash, []byte(password)); err != nil {
+		return auth.Claims{}, ErrAuthenticationFailure
+	}
+
+	// Finally after the above checks have been successful, genereate user token.
+	claims := auth.NewClaims(u.ID, u.Roles, now, time.Hour)
+	return claims, nil
 }
