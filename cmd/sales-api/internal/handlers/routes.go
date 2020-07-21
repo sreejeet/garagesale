ba258d85e77bed65b3b3a5be441dfdc41edc4800
V S Sreejeet <vsreejeet@gmail.com>
2020-07-21 17:14:08 +0530

    Authentication
    Modified all product and sale related routes to use authentication

--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -35,21 +35,23 @@ func API(db *sqlx.DB, log *log.Logger, authenticator *auth.Authenticator) http.H
 	}
 
 	{
+		// All handlers inside this block must be authenticated using tokens
+
 		p := Products{
 			db:  db,
 			log: log,
 		}
 
 		// Product specific routes
-		app.Handle(http.MethodGet, "/v1/products", p.List)
-		app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
-		app.Handle(http.MethodPost, "/v1/products", p.Create)
-		app.Handle(http.MethodPut, "/v1/products/{id}", p.Update)
-		app.Handle(http.MethodDelete, "/v1/products/{id}", p.Delete)
+		app.Handle(http.MethodGet, "/v1/products", p.List, mid.Authenticate(authenticator))
+		app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve, mid.Authenticate(authenticator))
+		app.Handle(http.MethodPost, "/v1/products", p.Create, mid.Authenticate(authenticator))
+		app.Handle(http.MethodPut, "/v1/products/{id}", p.Update, mid.Authenticate(authenticator))
+		app.Handle(http.MethodDelete, "/v1/products/{id}", p.Delete, mid.Authenticate(authenticator))
 
 		// Sale specific routes
-		app.Handle(http.MethodPost, "/v1/products/{id}/sales", p.AddSale)
-		app.Handle(http.MethodGet, "/v1/products/{id}/sales", p.ListSales)
+		app.Handle(http.MethodPost, "/v1/products/{id}/sales", p.AddSale, mid.Authenticate(authenticator))
+		app.Handle(http.MethodGet, "/v1/products/{id}/sales", p.ListSales, mid.Authenticate(authenticator))
 	}
 
 	return app
