1d4c7227d69cea984341b96bf43de93aa8d72bc8
V S Sreejeet <vsreejeet@gmail.com>
2020-07-22 22:00:53 +0530

    Authorization
    Middleware to check role

--- internal/mid/auth.go
+++ internal/mid/auth.go
@@ -2,12 +2,19 @@ package mid
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"strings"
 
-	"github.com/pkg/errors"
 	"github.com/sreejeet/garagesale/internal/platform/auth"
 	"github.com/sreejeet/garagesale/internal/platform/web"
+)
+
+// ErrForbidden indicates an authenticated trying to use actions that are
+// not accessible to their current role.
+var ErrForbidden = web.NewRequestError(
+	errors.New("you are unauthorized for that action"),
+	http.StatusForbidden,
 )
 
 // Authenticate middleware validates the token in the Authorization header.
@@ -33,6 +40,33 @@ func Authenticate(authenticator *auth.Authenticator) web.Middleware {
 
 			// Add claims to the context so they can be retrieved later.
 			ctx = context.WithValue(ctx, auth.Key, claims)
+
+			return after(ctx, w, r)
+		}
+
+		return h
+	}
+
+	return f
+}
+
+// HasRole validates that an authenticated user has at least one role from a
+// specified list. This method constructs the actual function that is used.
+func HasRole(roles ...string) web.Middleware {
+
+	// This is the actual middleware function to be executed.
+	f := func(after web.Handler) web.Handler {
+
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+			claims, ok := ctx.Value(auth.Key).(auth.Claims)
+			if !ok {
+				return errors.New("claims missing from context: HasRole called without/before Authenticate")
+			}
+
+			if !claims.HasRole(roles...) {
+				return ErrForbidden
+			}
 
 			return after(ctx, w, r)
 		}
