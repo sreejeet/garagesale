2fd31a8490e9758df662bdbc875b7b3f6dfae913
V S Sreejeet <vsreejeet@gmail.com>
2020-07-26 16:52:06 +0530

    added zipkin tracer for sales api

--- cmd/sales-api/main.go
+++ cmd/sales-api/main.go
@@ -9,18 +9,24 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+
 	"syscall"
 	"time"
+
+	openzipkin "github.com/openzipkin/zipkin-go"
+	zipkinHTTP "github.com/openzipkin/zipkin-go/reporter/http"
 
 	_ "expvar"         // Register expvars handlers
 	_ "net/http/pprof" // Register pprof handlers
 
+	"contrib.go.opencensus.io/exporter/zipkin"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/pkg/errors"
 	"github.com/sreejeet/garagesale/cmd/sales-api/internal/handlers"
 	"github.com/sreejeet/garagesale/internal/platform/auth"
 	"github.com/sreejeet/garagesale/internal/platform/conf"
 	"github.com/sreejeet/garagesale/internal/platform/database"
+	"go.opencensus.io/trace"
 )
 
 func main() {
@@ -56,6 +62,11 @@ func run() error {
 			KeyID          string `conf:"default:1"`
 			PrivateKeyFile string `conf:"default:private.pem"`
 			Algorithm      string `conf:"default:RS256"`
+		}
+		Trace struct {
+			URL         string  `conf:"default:http://localhost:9411/api/v2/spans"`
+			Service     string  `conf:"default:sales-api"`
+			Probability float64 `conf:"default:1"`
 		}
 	}
 
@@ -97,6 +108,19 @@ func run() error {
 		return errors.Wrap(err, "opening database")
 	}
 	defer db.Close()
+
+	// Start Tracing Support
+
+	closer, err := registerTracer(
+		cfg.Trace.Service,
+		cfg.Web.Address,
+		cfg.Trace.URL,
+		cfg.Trace.Probability,
+	)
+	if err != nil {
+		return err
+	}
+	defer closer()
 
 	// Start Debug Service
 	//
@@ -178,4 +202,21 @@ func createAuth(privateKeyFile, keyID, algorithm string) (*auth.Authenticator, e
 	public := auth.NewSimpleKeyLookupFunc(keyID, key.Public().(*rsa.PublicKey))
 
 	return auth.NewAuthenticator(key, keyID, algorithm, public)
+}
+
+// registerTracer is used to register a tracer for a particular service
+func registerTracer(service, httpAddr, traceURL string, probability float64) (func() error, error) {
+
+	localEndpoint, err := openzipkin.NewEndpoint(service, httpAddr)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating the local zipkinEndpoint")
+	}
+	reporter := zipkinHTTP.NewReporter(traceURL)
+
+	trace.RegisterExporter(zipkin.NewExporter(reporter, localEndpoint))
+	trace.ApplyConfig(trace.Config{
+		DefaultSampler: trace.ProbabilitySampler(probability),
+	})
+
+	return reporter.Close, nil
 }
